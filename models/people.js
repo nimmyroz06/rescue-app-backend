ac659bd9221dd5803347aec41ecf0ed565bb9d45
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-08-12 21:59:26 +0530

    created

---
+++ models/people.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const peopleSchema = mongoose.Schema(
+    {
+        name:{type:String,required:true},
+        phno:{type:String,required:true},
+        village:{type:String,required:true},
+        place:{type:String,required:true},
+        pin:{type:String,required:true},
+        houseno:{type:String,required:true},
+        missingdate:{type:String,required:true},
+        aadharno:{type:String,required:true},
+        gender:{type:String,required:true},
+        age:{type:String,required:true}
+    }
+)
+const peopleModel = mongoose.model("peoples",peopleSchema)
+module.exports=peopleModel
