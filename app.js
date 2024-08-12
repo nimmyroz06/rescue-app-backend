c80a2f6b4866f3eba23cfd18c61c38e078a74c16
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-08-12 21:44:37 +0530

    created

---
+++ app.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const cors = require("cors")
+const express = require("express")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const loginModel = require("./models/admin")
+const app = express()
+app.use(cors())
+app.use(express.json())
+mongoose.connect("")
+
+
+
+app.listen(8080,()=>{
+    console.log("server stated")
+})
